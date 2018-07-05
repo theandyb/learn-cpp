5eed309d84330c129c5bbcf986de19f826c08a38
Andy Beck <beckandy@umich.edu>
2018-07-05 15:19:26 -0400

    Initial commit

---
+++ basics/src/helloWorld.cpp
@@ -0,0 +1,10 @@
+// A simple "Hello, world!" application
+// Testing my (basic) knowledge of make
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << endl;
+	return 0;
+}

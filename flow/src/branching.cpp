e0407f6075bb4d84c99515dd3bd867d8b5e6408f
theandyb <beckandy@umich.edu>
2018-07-06 23:06:33 -0400

    Added a switch example to branching.cpp

--- flow/src/branching.cpp
+++ flow/src/branching.cpp
@@ -16,5 +16,18 @@ int main() {
 	std::cout.setf(std::ios::showpoint);         
 	std::cout.precision(2);
 	std::cout << "Homeboy worked " << hours << " hours this week and brought home $" << totalPay << std::endl;
+
+	// let's SWITCH things up a bit
+	switch (hours)
+	{
+		case 51:
+			std::cout << "Woah, 51 hours ON-THE-DOT???\n";
+			break;
+		case 13:
+			std::cout << "What an unlucky number of hours to have worked!\n";
+			break;
+		default:
+			std::cout << "That's nice, I suppose, but I've got nothing specific to say to you\n";
+	}
 	return 0;
 }

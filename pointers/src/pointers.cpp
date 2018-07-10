c6316adfc014cde2bac64cd8c7fb4c2175641563
Andy Beck <beckandy@umich.edu>
2018-07-10 12:57:39 -0400

    example of using pointer to change value stored at location

--- pointers/src/pointers.cpp
+++ pointers/src/pointers.cpp
@@ -4,5 +4,7 @@ int main() {
 	int number = 27;
 	int* ptrNumber = &number;
 	std::cout << "The value of the number stored at " << ptrNumber << " is " << *ptrNumber << std::endl;
+	*ptrNumber = 48;
+	std::cout << "We have changed the value stored at " << ptrNumber << " to " << number << std::endl;
 	return 0;
 }

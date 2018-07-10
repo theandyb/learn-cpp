4bf6d476163a829f8b71323cf0a97f0932124989
Andy Beck <beckandy@umich.edu>
2018-07-10 13:01:24 -0400

    Add warning comment

--- pointers/src/pointers.cpp
+++ pointers/src/pointers.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 	int number = 27;
-	int* ptrNumber = &number;
+	int* ptrNumber = &number; //ALWAYS initialize a pointer with the address of an existing variable or NULL
 	std::cout << "The value of the number stored at " << ptrNumber << " is " << *ptrNumber << std::endl;
 	*ptrNumber = 48;
 	std::cout << "We have changed the value stored at " << ptrNumber << " to " << number << std::endl;

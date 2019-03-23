075d6f678418626fdc4f6e20f37e4a8fa219d9a5
Andy Beck <andy@umich.edu>
2019-03-23 11:57:20 -0400

    Added more stuff to class

--- classes/src/Sales_data.h
+++ classes/src/Sales_data.h
@@ -5,7 +5,46 @@
 
 struct Sales_data {
     std::string bookNo;
+    std::string isbn() const {return this->bookNo;};
+    Sales_data& combine(const Sales_data&);
     unsigned units_sold = 0;
     double revenue = 0.0;
+    double avg_price() const;
 };
+
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+double Sales_data::avg_price() const {
+    if(units_sold){
+        return revenue/units_sold;
+    } else
+        return 0;
+}
+
+Sales_data& Sales_data::combine(const Sales_data& rhs){
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+std::istream &read(std::istream &is, Sales_data &item){
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item){
+    os << item.isbn() << " " << item.units_sold << " "
+        << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs){
+    Sales_data total = lhs;
+    total.combine(rhs);
+    return total;
+}
 #endif
--- classes/src/class1.cpp
+++ classes/src/class1.cpp
@@ -2,39 +2,14 @@
 #include<string>
 #include "Sales_data.h"
 
-void printC(Sales_data &x)
-{
-    std::cout << x.units_sold << " copies of " << x.bookNo << " were sold at an average price of $"
-        << x.revenue / static_cast<double>(x.units_sold) << std::endl;
-}
-
 int main(){
-    Sales_data data1, data2;
-    double price = 0;
+    Sales_data a, b;
 
-    std::cout << "ISBN: ";
-    std::cin >> data1.bookNo;
-    std::cout << "Number sold: ";
-    std::cin >> data1.units_sold; 
-    std::cout << "At what price?: ";
-    std::cin >> price;
-    data1.revenue = data1.units_sold * price;
-    
-    std::cout << "ISBN: ";
-    std::cin >> data2.bookNo;
-    std::cout << "Number sold: ";
-    std::cin >> data2.units_sold; 
-    std::cout << "At what price?: ";
-    std::cin >> price;
-    data2.revenue = data2.units_sold * price;
+    std::cout << "Enter isbn, units sold, and price (hitting enter after each)" << std::endl;
+    read(std::cin, a);
 
-    if(data2.bookNo == data1.bookNo){
-        std::cout << (data1.units_sold + data2.units_sold) 
-            << " copies of " << data1.bookNo << " were sold at an average price of $" 
-            << (data1.revenue + data2.revenue) / static_cast<double>(data1.units_sold + data2.units_sold) 
-            << std::endl;
-    } else {
-        printC(data1);
-        printC(data2);
-    }
+    std::cout << "Enter isbn, units sold, and price (hitting enter after each)" << std::endl;
+    read(std::cin, b);
+
+    print(std::cout, a);
 }
